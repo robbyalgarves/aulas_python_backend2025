758fa6d25efc73fba451d1a6e3b6fdfe9e829ae4
Roberta <roberta@gmail.com>
2025-10-14 21:30:44 -0300

    Desafio fim de semana

---
+++ desafio14_10.py
@@ -0,0 +1,38 @@
+import interface
+from interface import dobro, triplo, quadrado
+
+start = input("Digite sim para iniciar ou não para parar: ")
+
+def menu():
+
+    while True:
+        escolha = int(input('''Digite a opção desejada: "
+        1- Calcular o dobro
+        2- Calcular o triplo 
+        3- Calcular o quadrado 
+        4- para SAIR: '''
+        ))
+
+        if  escolha == 1:
+            numero = int(input("Digite um número: "))
+            dobro = interface.dobro(numero)
+            print(f"O dobro de {numero} é {dobro}")
+
+        if escolha == 2:
+            numero1 = int(input("Digite um número: "))
+            triplo = interface.triplo(numero1)
+            print(f"O triplo de {numero1} é {triplo}")
+            
+        elif escolha == 3:
+            numero2 = int(input("Digite um número: "))
+            quadrado = interface.quadrado(numero2)
+            print(f"O quadrado de {numero2} é {quadrado}")
+
+        elif escolha == 4:
+            print("SAIR")
+            break
+        else:
+            print("Digite o número correto!")
+
+if start == "sim":
+    menu()
---
+++ interface.py
@@ -0,0 +1,8 @@
+def dobro(numero):
+    return int(numero) * 2
+
+def triplo(numero1):
+    return int(numero1) * 3
+
+def quadrado(numero2):
+    return int(numero2) * 4
